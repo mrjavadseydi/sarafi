fe0d4d6246929905bf69251b0e18a80b9aba1009
MrJavad <seydi.birjand@gmail.com>
2021-08-30 09:44:15 +0430

    complete switchs

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -7,55 +7,80 @@ use Illuminate\Http\Request;
 
 class TelegramController extends ActivationController
 {
-    public $message_type ;
-    public $text ;
+    public $message_type;
+    public $text;
     public $chat_id;
     public $from_id;
     public $user;
-    public function index(Request $request){
+
+    public function index(Request $request)
+    {
         $req = $request->toArray();
         $this->message_type = messageType($req);
-        if ($this->message_type== "callback_query"){
+        if ($this->message_type == "callback_query") {
             $this->initCallBack($req);
             exit();
         }
-        $this->text = $req['message']['text']?? "//**";
+        $this->text = $req['message']['text'] ?? "//**";
         $this->chat_id = isset($req['message']['chat']['id']) ?? $req['message']['chat']['id'];
         $this->from_id = isset($req['message']['from']['id']) ?? $req['message']['from']['id'];
-        if(! ($user = Member::where('chat_id',$this->chat_id)->first())){
+        if (!($user = Member::where('chat_id', $this->chat_id)->first())) {
             $user = Member::create([
-                'chat_id'=>$this->chat_id
+                'chat_id' => $this->chat_id
             ]);
         }
         $this->user = $user;
-        if(!joinCheck($this->chat_id,getConfig('channel'))){
+        if (!joinCheck($this->chat_id, getConfig('channel'))) {
             sendMessage([
-                'chat_id'=>$this->chat_id,
-                'text'=>getConfig('join_text'),
-                'reply_markup'=> joinKey()
+                'chat_id' => $this->chat_id,
+                'text' => getConfig('join_text'),
+                'reply_markup' => joinKey()
             ]);
             return 0;
         }
-        if($this->text=="/start"||$this->text=='بازگشت ↪️'){
+        if ($this->text == "/start" || $this->text == 'بازگشت ↪️') {
             $this->start();
             exit();
         }
-        switch ($this->user->state){
+        switch ($this->user->state) {
             case "SendPhone":
                 $this->receivePhone($req);
                 break;
-          case "SendNationalCode":
+            case "SendNationalCode":
                 $this->receiveNationalCode();
                 break;
-          case "SendCardPhoto":
-                $this->SendCardPhoto($req);
+            case "SendShaba":
+                $this->receiveShaba();
                 break;
-
+            case "SendCardNumber":
+                $this->receiveCard();
+                break;
+            case "SendCardPhoto":
+                $this->receiveCardPhoto($req);
+                break;
+            case "SendBankCardPhoto":
+                $this->receiveBankCardPhoto($req);
+                break;
+            case "SendVerificationVideo":
+                $this->receiveVerificationVideo($req);
+                break;
+            case  "GiveSellCount":
+                $this->receiveSellCount();
+                break;
+            case  "GiveResid":
+                $this->receiveResid($req);
+                break;
+            case  "GiveVoocherKey":
+                $this->receiveVoocherKey();
+                break;
+            case  "getActivator":
+                $this->receiveActivator();
+                break;
             default:
                 break;
         }
-        if ($this->message_type=="text"){
-            switch ($this->text){
+        if ($this->message_type == "text") {
+            switch ($this->text) {
                 case 'بازگشت ↪️' :
                 case "/start":
                     $this->start();
@@ -69,6 +94,27 @@ class TelegramController extends ActivationController
                 case "🟢 احراز هویت 🟢" :
                     $this->initActivate($req);
                     break;
+                case "📄 نرخ ارزها":
+                    $this->getPrice();
+                    break;
+                case "⁉️ سوالات متداول":
+                    $this->getFAQ();
+                    break;
+                case "📞 تماس با ما":
+                    $this->getContactUs();
+                    break;
+                case "💡 راهنما":
+                    $this->getHelp();
+                    break;
+                case "👮 قوانین و مقررات":
+                    $this->getRole();
+                    break;
+                case  "👤 پروفایل کاربری":
+                    $this->getProfile();
+                    break;
+                case "💸 خرید/فروش های من":
+                    $this->userTransaction();
+                    break;
                 default :
                     break;
 
@@ -76,12 +122,14 @@ class TelegramController extends ActivationController
 
         }
     }
-    public function start(){
+
+    public function start()
+    {
         nullState($this->chat_id);
         sendMessage([
-            'chat_id'=>$this->chat_id,
-            'text'=>"به منو اصلی خوش آمدید",
-            'reply_markup'=>menuButton()
+            'chat_id' => $this->chat_id,
+            'text' => "به منو اصلی خوش آمدید",
+            'reply_markup' => menuButton()
         ]);
     }
 
